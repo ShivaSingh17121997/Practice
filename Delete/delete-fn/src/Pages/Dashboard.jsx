3496389924252aaa265530eda89afd742d6d2fe2
ShivaSingh17121997 <shivasingh001997@gmail.com>
2023-11-14 07:40:22 +0530

    added

--- Delete/delete-fn/src/Pages/Dashboard.jsx
+++ Delete/delete-fn/src/Pages/Dashboard.jsx
@@ -46,35 +46,35 @@
 
 //--------------------------------------------------------------------------------------------------------------------------------------------
 
-import React from 'react'
+// import React from 'react'
 
-export default function Dashboard() {
-    const handleDelete = () => {
-        axios.delete(`url/${userId}`)
-        .then((res) => {
-            setRes((prevPost) => prevPost.filter((item) => item.id !== userId ) )
-        } )
-    }
+// export default function Dashboard() {
+//     const handleDelete = () => {
+//         axios.delete(`url/${userId}`)
+//         .then((res) => {
+//             setRes((prevPost) => prevPost.filter((item) => item.id !== userId ) )
+//         } )
+//     }
 
-    const handleDelete = () => {
-        axios.delete(`url/${userId}`)
-        .then((res) => {setRes((prevPost) => prevPost.filter((item)=> item.id !== userId))})
-    }
+//     const handleDelete = () => {
+//         axios.delete(`url/${userId}`)
+//         .then((res) => {setRes((prevPost) => prevPost.filter((item)=> item.id !== userId))})
+//     }
 
-    return (
-        <div>
-            {res?.map((item) => {
-                return <div style={{ border: "1px solid black", display: "grid", padding: "10px", margin: '40px' }}>
-                    <div key={item.id} >
-                        <p>{item.id}</p>
-                        <p>{item.title}</p>
-                        <button onClick={() => handleDelete(item.id)} >Delete</button>
-                    </div>
-                </div>
+//     return (
+//         <div>
+//             {res?.map((item) => {
+//                 return <div style={{ border: "1px solid black", display: "grid", padding: "10px", margin: '40px' }}>
+//                     <div key={item.id} >
+//                         <p>{item.id}</p>
+//                         <p>{item.title}</p>
+//                         <button onClick={() => handleDelete(item.id)} >Delete</button>
+//                     </div>
+//                 </div>
 
-            })}
+//             })}
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
