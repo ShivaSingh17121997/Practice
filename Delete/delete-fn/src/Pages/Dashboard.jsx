799fa1f3cd4e2c7475cc4d69affedd96fc08a19c
ShivaSingh17121997 <shivasingh001997@gmail.com>
2023-11-14 07:39:45 +0530

    added

--- Delete/delete-fn/src/Pages/Dashboard.jsx
+++ Delete/delete-fn/src/Pages/Dashboard.jsx
@@ -1,33 +1,68 @@
+// import React from 'react'
+// import { useEffect } from 'react'
+// import axios from "axios";
+// import { useState } from 'react';
+
+// export default function Dashboard() {
+//     const [res, setRes] = useState([])
+//     // const [del, setdel] = useState()
+
+//     const url = `https://jsonplaceholder.typicode.com/posts/`
+//     useEffect(() => {
+//         axios.get(url)
+//             .then((res) => {
+//                 setRes(res.data)
+//                 console.log(res)
+//             })
+//     }, [])
+
+//     // delete function
+//     const handleDelete = (userId) => {
+//         axios.delete(`https://jsonplaceholder.typicode.com/posts/${userId}`)
+//             .then((res) => {
+//                 console.log("Post deleted", res.data)
+//                 setRes((prevPost) => prevPost.filter((item) => item.id !== userId))
+//             })
+
+//     }
+
+//     return (
+//         <div   >
+//             {res?.map((item) => {
+//                 return <div style={{ border: "1px solid black", display: "grid", padding: "10px", margin: '40px' }}>
+//                     <div key={item.id} >
+//                         <p>{item.id}</p>
+//                         <p>{item.title}</p>
+//                         <button onClick={() => handleDelete(item.id)} >Delete</button>
+//                     </div>
+//                 </div>
+
+//             })}
+
+//         </div>
+//     )
+// }
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------
+
 import React from 'react'
-import { useEffect } from 'react'
-import axios from "axios";
-import { useState } from 'react';
 
 export default function Dashboard() {
-    const [res, setRes] = useState([])
-    // const [del, setdel] = useState()
+    const handleDelete = () => {
+        axios.delete(`url/${userId}`)
+        .then((res) => {
+            setRes((prevPost) => prevPost.filter((item) => item.id !== userId ) )
+        } )
+    }
 
-    const url = `https://jsonplaceholder.typicode.com/posts/`
-    useEffect(() => {
-        axios.get(url)
-            .then((res) => {
-                setRes(res.data)
-                console.log(res)
-            })
-    }, [])
-
-    // delete function
-    const handleDelete = (userId) => {
-        axios.delete(`https://jsonplaceholder.typicode.com/posts/${userId}`)
-            .then((res) => {
-                console.log("Post deleted", res.data)
-                setRes((prevPost) => prevPost.filter((item) => item.id !== userId))
-            })
-
+    const handleDelete = () => {
+        axios.delete(`url/${userId}`)
+        .then((res) => {setRes((prevPost) => prevPost.filter((item)=> item.id !== userId))})
     }
 
     return (
-        <div   >
+        <div>
             {res?.map((item) => {
                 return <div style={{ border: "1px solid black", display: "grid", padding: "10px", margin: '40px' }}>
                     <div key={item.id} >
@@ -43,5 +78,3 @@ export default function Dashboard() {
     )
 }
 
-
-//
